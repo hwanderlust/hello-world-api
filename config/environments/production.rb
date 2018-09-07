670c10f50adc7deccb7df379b5971468fadd0b99
Paul <lpault29@gmail.com>
2018-09-07 00:14:45 -0400

    env prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,9 +34,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.mount_path = '/websocket'
+  # config.action_cable.mount_path = '/websocket'
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.url = 'ws://hello-world-exchange.herokuapp.com/cable'
+  # config.action_cable.url = 'ws://hello-world-exchange.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = [ 'https://hello-world-exchange.herokuapp.com' ]
 
