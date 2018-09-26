febea08d9891f9a8861863f111264559e4c1b5df
Paul <lpault29@gmail.com>
2018-09-25 20:38:01 -0400

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,26 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
-  get 'lists/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
+
+  # get 'lists/create'
+  # resources :conversations, only: [:index, :create]
+  # resources :chats, only: [:index, :create, :show]
+
   resources :users, only: [:index, :create, :show]
-  resources :conversations, only: [:index, :create]
-  resources :chats, only: [:index, :create, :show]
-  resources :messages, only: [:create]
-  resources :lists, only: [:create, :show, :destroy]
   post '/login' => 'users#login'
   post '/get_user' => 'users#get_user'
+  get '/users/:id/lists' => 'users#get_lists'
+  
+  resources :chats, only: [:create, :show]
+
+  resources :messages, only: [:create]
   post '/translate' => 'messages#translate'
   post '/detect_language' => 'messages#detection'
-  get '/users/:id/lists' => 'users#get_lists'
   post '/save_message' => 'messages#add_to_list'
   patch '/save_message' => 'messages#add_to_list'
   post '/remove_message' => 'messages#remove_from_list'
 
+  resources :lists, only: [:create, :show, :destroy]
 
 end
